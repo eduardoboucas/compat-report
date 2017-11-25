2c6c5ad74b2d8d600c838af56dd3adbc9d0e2941
Eduardo Bouças <mail@eduardoboucas.com>
2017-11-25 23:53:11 +0000

    Add test suite

---
+++ test/StyleSheet.test.js
@@ -0,0 +1,19 @@
+const StyleSheet = require('./../src/StyleSheet')
+
+describe('StyleSheet', () => {
+  describe('constructor', () => {
+    test('creates the scaffolding for the browser versions provided', () => {
+      const mockBrowserList = {
+        chrome: ['1.0', '2.0', '3.0'],
+        safari: ['1.1', '2.2', '3.3']
+      }
+      const stylesheet = new StyleSheet(mockBrowserList)
+
+      Object.keys(mockBrowserList).forEach(browser => {
+        mockBrowserList[browser].forEach(version => {
+          expect(stylesheet.issues[browser][version]).toEqual([])
+        })
+      })
+    })
+  })
+})
